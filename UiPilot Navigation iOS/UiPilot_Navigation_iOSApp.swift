63eef099919d47fb6ae375e962dc50c3911485b6
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-02-09 10:59:57 +0600

    Initial Commit

---
+++ UiPilot Navigation iOS/UiPilot_Navigation_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UiPilot_Navigation_iOSApp.swift
+//  UiPilot Navigation iOS
+//
+//  Created by Rezaul Islam on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct UiPilot_Navigation_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
