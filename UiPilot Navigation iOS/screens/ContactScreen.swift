cc8995920f899896ee0e9dc1718c79625069fcb6
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-02-09 19:16:06 +0600

    Navigation for ios 15 swiftui

---
+++ UiPilot Navigation iOS/screens/ContactScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ContactScreen.swift
+//  UiPilot Navigation iOS
+//
+//  Created by Rezaul Islam on 9/2/24.
+//
+
+import SwiftUI
+
+struct ContactScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactScreen()
+}
---
+++ UiPilot Navigation iOS/screens/ProjectDetailsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  Project.swift
+//  UiPilot Navigation iOS
+//
+//  Created by Rezaul Islam on 9/2/24.
+//
+
+import SwiftUI
+
+struct ProjectDetailsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProjectDetailsScreen()
+}
